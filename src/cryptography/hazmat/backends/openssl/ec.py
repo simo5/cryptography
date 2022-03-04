617c66e2b9dcc311c2e5221b50a45ec99f5d0c67
Simo Sorce <simo@redhat.com>
2022-03-04 12:05:50 -0500

    Convert ECDSA sign() to use EVP_DigestSign()
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- src/cryptography/hazmat/backends/openssl/ec.py
+++ src/cryptography/hazmat/backends/openssl/ec.py
@@ -15,6 +15,7 @@ from cryptography.hazmat.backends.openssl.utils import (
 )
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.asymmetric.utils import Prehashed
 
 if typing.TYPE_CHECKING:
     from cryptography.hazmat.backends.openssl.backend import Backend
@@ -80,21 +81,6 @@ def _sn_to_elliptic_curve(backend: "Backend", sn: str) -> ec.EllipticCurve:
             "{} is not a supported elliptic curve".format(sn),
             _Reasons.UNSUPPORTED_ELLIPTIC_CURVE,
         )
-
-
-def _ecdsa_sig_sign(
-    backend: "Backend", private_key: "_EllipticCurvePrivateKey", data: bytes
-) -> bytes:
-    max_size = backend._lib.ECDSA_size(private_key._ec_key)
-    backend.openssl_assert(max_size > 0)
-
-    sigbuf = backend._ffi.new("unsigned char[]", max_size)
-    siglen_ptr = backend._ffi.new("unsigned int[]", 1)
-    res = backend._lib.ECDSA_sign(
-        0, data, len(data), sigbuf, siglen_ptr, private_key._ec_key
-    )
-    backend.openssl_assert(res == 1)
-    return backend._ffi.buffer(sigbuf)[: siglen_ptr[0]]
 
 
 def _ecdsa_sig_verify(
@@ -195,11 +181,54 @@ class _EllipticCurvePrivateKey(ec.EllipticCurvePrivateKey):
         signature_algorithm: ec.EllipticCurveSignatureAlgorithm,
     ) -> bytes:
         _check_signature_algorithm(signature_algorithm)
-        data, _ = _calculate_digest_and_algorithm(
-            data,
-            signature_algorithm.algorithm,
+        evp_md_ctx = self._backend._lib.EVP_MD_CTX_new()
+        self._backend.openssl_assert(evp_md_ctx != self._backend._ffi.NULL)
+        evp_md_ctx = self._backend._ffi.gc(
+            evp_md_ctx, self._backend._lib.EVP_MD_CTX_free
         )
-        return _ecdsa_sig_sign(self._backend, self, data)
+        if isinstance(signature_algorithm.algorithm, Prehashed):
+            if len(data) != signature_algorithm._algorithm.digest_size:
+                raise ValueError(
+                    "The provided data must be the same length as the hash "
+                    "algorithm's digest size."
+                )
+            evp_md = self._backend._ffi.NULL
+        else:
+            evp_md = self._backend._evp_md_from_algorithm(
+                signature_algorithm.algorithm
+            )
+            if evp_md == self._backend._ffi.NULL:
+                raise UnsupportedAlgorithm(
+                    "{} is not a supported hash on this backend.".format(
+                        algorithm.name
+                    ),
+                    _Reasons.UNSUPPORTED_HASH,
+                )
+
+        res = self._backend._lib.EVP_DigestSignInit(
+            evp_md_ctx,
+            self._backend._ffi.NULL,
+            evp_md,
+            self._backend._ffi.NULL,
+            self._evp_pkey,
+        )
+        self._backend.openssl_assert(res == 1)
+        res = self._backend._lib.EVP_DigestSignUpdate(
+            evp_md_ctx, data, len(data)
+        )
+        self._backend.openssl_assert(res == 1)
+        siglen_ptr = self._backend._ffi.new("size_t *", 0)
+        res = self._backend._lib.EVP_DigestSignFinal(
+            evp_md_ctx, self._backend._ffi.NULL, siglen_ptr
+        )
+        self._backend.openssl_assert(res == 1)
+        self._backend.openssl_assert(siglen_ptr[0] > 0)
+        sigbuf = self._backend._ffi.new("unsigned char[]", siglen_ptr[0])
+        res = self._backend._lib.EVP_DigestSignFinal(
+            evp_md_ctx, sigbuf, siglen_ptr
+        )
+        self._backend.openssl_assert(res == 1)
+        return self._backend._ffi.buffer(sigbuf)[: siglen_ptr[0]]
 
 
 class _EllipticCurvePublicKey(ec.EllipticCurvePublicKey):
